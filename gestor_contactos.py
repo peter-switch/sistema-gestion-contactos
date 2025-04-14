74e80072fb9d4af756e04f1d7db132c14a81404e
peter-switch <202387707+peter-switch@users.noreply.github.com>
2025-04-14 10:49:36 +0200

    gestorv01

--- gestor_contactos.py
+++ gestor_contactos.py
@@ -1,3 +1,5 @@
+
+#Clase para generar contactos
 class Contacto:
     def __init__(self, nombre, telefono, correo):
         self.nombre = nombre
@@ -7,7 +9,11 @@ class Contacto:
     def __str__(self):
         return f"Contacto: {self.nombre} | {self.telefono} | {self.correo}\n"
     
-
+    #
+    def insercion_contacto(self):
+        return f"{self.nombre},{self.telefono},{self.correo}\n"
+    
+#Clase para generar los métodos de gestión
 class GestionContactos:
 
     def __init__(self):
@@ -24,4 +30,9 @@ class GestionContactos:
     
     def eliminar_contactos(self):
         pass
+
+#Clase para generar el menú interactivo
+class MenuApp:
+
+    pass
   
